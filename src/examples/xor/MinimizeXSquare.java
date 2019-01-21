f38461a1e5b830a2a0eb77496e41fdded0a25b2d
valentin616 <valchap616@gmail.com>
2019-01-21 21:42:10 +0100

    Add an other example and comments

---
+++ src/examples/xor/MinimizeXSquare.java
@@ -0,0 +1,44 @@
+package examples.xor;
+
+import reverseGraph.Graph;
+import reverseGraph.nodes.Param;
+import reverseGraph.nodes.operations.Multiplication;
+import reverseGraph.nodes.operations.Operation;
+import reverseGraph.optimizers.Optimizer;
+import reverseGraph.optimizers.SGD;
+
+/**
+ * Example that optimizes the x^2 function
+ */
+public class MinimizeXSquare {
+	public static void main(String[] args) {
+		
+		// Create a parameter and set its value to 5
+		Param x = new Param(5);
+		
+		// Create an operation that squares that parameter
+		Operation square = new Multiplication(x, x);
+		
+		// Create an optimizer to update the parameter by 0.2 time the derivative
+		Optimizer optimizer = new SGD(0.2);
+		
+		// Create a graph to compute and optimize the function x^2
+		Graph graph = new Graph(square, optimizer);
+		
+		for(int i = 0; i < 10; i++) {
+			// Compute the value of x^2
+			graph.compute();
+			
+			// Compute the derivatives of the function
+			graph.computeDerivatives();
+			
+			// Update the parameter by removing 0.2 time the derivative
+			graph.minimize();
+			
+			// Print the parameter and function output values
+			System.out.println("X value : " + x.getOutput());
+			System.out.println("Function output : " + square.getOutput());
+			System.out.println();
+		}
+	}
+}
--- src/examples/xor/XOR.java
+++ src/examples/xor/XOR.java
@@ -1,77 +1,101 @@
 package examples.xor;
 
-import java.util.ArrayList;
-
 import reverseGraph.Graph;
 import reverseGraph.nodes.Input;
 import reverseGraph.nodes.operations.Multiplication;
 import reverseGraph.nodes.operations.Operation;
 import reverseGraph.nodes.operations.Substraction;
-import reverseGraph.optimizers.RMSprop;
+import reverseGraph.optimizers.Adam;
 import vnl.Activation;
 import vnl.Network;
 import vnl.model.LayerModel;
 
+/*
+ * Neural network learning XOR logical gate
+ */
 public class XOR {
 	public static void main(String[] args) {
-		Input i1 = new Input(0);
-		Input i2 = new Input(0);
+		// Net inputs
+		Input input1 = new Input(0);
+		Input input2 = new Input(0);
 
-		ArrayList<LayerModel> layers = new ArrayList<>();
-		layers.add(new LayerModel(100, Activation.LEAKYRELU));
-		layers.add(new LayerModel(100, Activation.LEAKYRELU));
-		layers.add(new LayerModel(1, Activation.TANH));
+		// Create net model
+		LayerModel[] layers = new LayerModel[2];
+		layers[0] = new LayerModel(10, Activation.LEAKYRELU);
+		layers[1] = new LayerModel(1, Activation.TANH);
 
-		Operation output = Network.createNetwork(new Input[] { i1, i2 }, layers.toArray(new LayerModel[0]))[0];
+		// Create net
+		Operation output = Network.createNetwork(new Input[] { input1, input2 }, layers)[0];
 
+		// Net label (the desired output)
 		Input label = new Input(0);
+		
+		// Add operation to compute the difference between the current and the desired output
 		Substraction diff = new Substraction(output, label);
 		Multiplication loss = new Multiplication(diff, diff);
 
-		System.out.println("Creating graph");
-
-		Graph graph = new Graph(loss, new RMSprop());
-
-		System.out.println("Graph created");
+		// Create a graph to compute all operations
+		Graph graph = new Graph(loss, new Adam(0.03, 0.9, 0.999));
 
 		double errorSum = 1;
 
 		while (errorSum > 0.1) {
 			errorSum = 0;
 
-			i1.setValue(-1);
-			i2.setValue(-1);
+			// Set input for 0 XOR 0 = 0
+			input1.setValue(-1);
+			input2.setValue(-1);
 			label.setValue(-1);
+			
+			// Compute net output
 			graph.compute();
-			graph.computeDerivatives();
 			errorSum += loss.getOutput();
+			
+			// Learn the current input
+			graph.computeDerivatives();
 			graph.minimize();
 
-			i1.setValue(1);
-			i2.setValue(-1);
+			// Set input for 1 XOR 0 = 1
+			input1.setValue(1);
+			input2.setValue(-1);
 			label.setValue(1);
+			
+			// Compute net output
 			graph.compute();
-			graph.computeDerivatives();
 			errorSum += loss.getOutput();
+			
+			// Learn the current input
+			graph.computeDerivatives();
 			graph.minimize();
-
-			i1.setValue(-1);
-			i2.setValue(1);
+			
+			// Set input for 0 XOR 1 = 1
+			input1.setValue(-1);
+			input2.setValue(1);
 			label.setValue(1);
+			
+			// Compute net output
 			graph.compute();
-			graph.computeDerivatives();
 			errorSum += loss.getOutput();
+			
+			// Learn the current input
+			graph.computeDerivatives();
+			graph.minimize();
+			
+			// Set input for 1 XOR 1 = 1
+			input1.setValue(1);
+			input2.setValue(1);
+			label.setValue(1);
+			
+			// Compute net output
+			graph.compute();
+			errorSum += loss.getOutput();
+			
+			// Learn the current input
+			graph.computeDerivatives();
 			graph.minimize();
 
-			i1.setValue(1);
-			i2.setValue(1);
-			label.setValue(-1);
-			graph.compute();
-			graph.computeDerivatives();
-			errorSum += loss.getOutput();
-			graph.minimize();
+			// Print the error of the net
 			System.out.println(errorSum);
 		}
-		System.out.println("2.655399949887283E-8");
 	}
 }
